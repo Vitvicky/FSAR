a73259a66ec25ac24722a5d0e873b2f2be05eb26
ZHUOYI WANG <sduwzy1992@gmail.com>
2020-06-09 10:02:40 -0500

    Add files via upload

---
+++ model/loss/adversarial_loss.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+
+
+def confuse_data(x, y, alpha=0.2, use_cuda=True):
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def confuse_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
